7e3138452b6d82310a0bf02a844fce983c94c0e2
Ankur083 <ankurjpl07@gmail.com>
2026-08-22 15:49:09 +0530

    Time: 1 ms (98.74%), Space: 48 MB (5.43%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.java
+++ 0605-can-place-flowers/0605-can-place-flowers.java
@@ -5,36 +5,23 @@ class Solution {
         if(n == 1 && arr.length == 1 && arr[0] == 0) return true;
 
         for(int i = 0; i < arr.length; i++){
-            if(i == 0){
-                if(arr[i] == 1){
-                    continue;
+            if(arr[i] == 0){
+                if(i == 0 && i+1 < arr.length && arr[i+1] == 0){
+                    arr[i] = 1;
+                    n--;
+                }
+                else if(i == arr.length-1 && i-1 >= 0 && arr[i-1] == 0){
+                    arr[i] = 1;
+                    n--;
                 }
                 else{
-                    if(i != arr.length-1 && arr[i+1] != 1){
+                    if( i-1 >= 0 && arr[i-1] == 0 && i+1 < arr.length && arr[i+1] == 0){
                         arr[i] = 1;
                         n--;
                     }
-                          
                 }
             }
-            else if(i != arr.length-1){
-                if(arr[i] == 1){
-                    continue;
-                }
-                else if(arr[i-1] != 1 && arr[i+1] != 1){
-                    arr[i] = 1;
-                    n--;
-                }
-            }
-            else{
-                if(arr[i] == 0 && i-1 >= 0 && arr[i-1] == 0){
-                    arr[i] = 1;
-                    n--;
-                }
-                
-            }
-
-            if(n == 0) return true;
+            if(n == 0) return true; 
         }
         return false;
     }
