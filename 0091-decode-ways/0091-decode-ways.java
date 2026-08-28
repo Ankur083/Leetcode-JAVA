2bcd2cffd9a33bac65e460ea6cb226f7179f72cd
Ankur Raj <ankurjpl07@gmail.com>
2026-08-28 16:22:42 +0530

    Time: 3 ms (4.73%), Space: 46.4 MB (7.63%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,68 +1,43 @@
 class Solution {
-    // public int cntOfDecoded(int i, int j, Set<String>set, String s){
+    public int cntOfDecoded(int i, int j, Set<String> set, String s, int [][]dp) {
 
-    //     if(j != 0 && (j > s.length() || !set.contains(s.substring(i, j)))){
-    //         return 0;
-    //     }
-
-    //     if(j == s.length()){
-    //         return 1;
-    //     }
-
-    //     int a = 0;
-    //     int b = 0;
-
-    //      a = cntOfDecoded(j, j+1, set, s);
-    //      b = cntOfDecoded(j, j+2, set, s);
-
-    //     return a+b;
-    // }
-
-    public int cntOfDecoded(int i, Set<String> st, String s, int []dp) {
-        int n = s.length();
-
-        if(i > n)
+        if (j > s.length()) {
             return 0;
-        
-        if(i == n)
+        }
+
+        if (!set.contains(s.substring(i, j))) {
+            return 0;
+        }
+
+        if (j == s.length()) {
             return 1;
-
-        if(dp[i] != -1){
-            return dp[i];
         }
 
-        String one = "" + s.charAt(i);
-        int a=0, b = 0;
-
-        if(st.contains(one)) {
-            a = cntOfDecoded(i+1, st, s, dp);
+        if(dp[i][j] != -1){
+            return dp[i][j];
         }
 
-        String two = "";
-        if(i+1 < n) {
-            two = ""  + s.charAt(i) + s.charAt(i+1);
-        }
+        int a = cntOfDecoded(j, j + 1, set, s, dp);
+        int b = cntOfDecoded(j, j + 2, set, s, dp);
 
-        if(st.contains(two)) {
-            b = cntOfDecoded(i+2, st, s, dp);
-        }
-
-        return dp[i] = a+b;
+        return dp[i][j] = a + b;
     }
 
     public int numDecodings(String s) {
-        Set<String>set = new HashSet<>();
+        Set<String> set = new HashSet<>();
 
-        for(int i = 1; i <= 26; i++){
-            String str = ""+i;
+        for (int i = 1; i <= 26; i++) {
+            String str = "" + i;
             set.add(str);
         }
+
         int n = s.length();
-        int dp[] = new int[n];
+        int[][] dp = new int[n + 1][n + 2];
 
-        Arrays.fill(dp, -1);
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
 
-
-        return cntOfDecoded(0, set, s, dp);
+        return cntOfDecoded(0, 1, set, s, dp) + cntOfDecoded(0, 2, set, s, dp);
     }
 }
