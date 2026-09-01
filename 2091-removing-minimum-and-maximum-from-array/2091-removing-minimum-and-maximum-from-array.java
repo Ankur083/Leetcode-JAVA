2089b1f9b4fe3ea2208b83c64b4b839e6dd80eb3
Ankur Raj <ankurjpl07@gmail.com>
2026-09-01 09:35:27 +0530

    Time: 2 ms (100%), Space: 86.8 MB (49.75%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -3,72 +3,90 @@ class Solution {
         
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
+        int idx1 = -1;
+        int idx2 = -1;
 
 
-        for(int num :nums){
-            max = Math.max(max, num);
-            min = Math.min(min, num);
+        for(int i = 0; i < nums.length ;i++){
+
+            if(max < nums[i]){
+                idx1 = i;
+                max = nums[i];
+            }
+            if(min > nums[i]){
+                idx2 = i;
+                min = nums[i];
+            }
+           
         }
 
         int ans = nums.length;
 
-        int cnt = 0;
-        int flag = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == min || nums[i] == max){
-                cnt++;
-                flag++;
-                if(flag == 2){
-                    break;
-                }
-            }
-            else{
-                cnt++;
-            }
-        }
+        int cntL = Math.max(idx1, idx2)+1;
+        int cntR = nums.length-Math.min(idx1, idx2);
+        int cnt = Math.min(idx1, idx2)+1 + (nums.length-Math.max(idx1, idx2));
 
-        ans = Math.min(ans , cnt);
-        cnt = 0;
-        flag = 0;
 
-        for(int i = nums.length-1; i >= 0; i--){
-            if(nums[i] == min || nums[i] == max){
-                cnt++;
-                flag++;
-                if(flag == 2){
-                    break;
-                }
-            }
-            else{
-                cnt++;
-            }
-        }
-        ans = Math.min(ans , cnt);
-        cnt = 0;
-        int cnt1 = 0;
+        return Math.min(cntL, Math.min(cnt, cntR));
+        
 
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == min || nums[i] == max){
-                cnt++;
-                break;
-            }
-            else{
-                cnt++;
-            }
-        }
+        // int cnt = 0;
+        // int flag = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] == min || nums[i] == max){
+        //         cnt++;
+        //         flag++;
+        //         if(flag == 2){
+        //             break;
+        //         }
+        //     }
+        //     else{
+        //         cnt++;
+        //     }
+        // }
 
-        for(int i = nums.length-1; i >= 0; i--){
-            if(nums[i] == min || nums[i] == max){
-                cnt1++;
-                break;    
-            }
-            else{
-                cnt1++;
-            }
-        }
+        // ans = Math.min(ans , cnt);
+        // cnt = 0;
+        // flag = 0;
 
-        ans = Math.min(ans, cnt+cnt1);
-        return ans;
+        // for(int i = nums.length-1; i >= 0; i--){
+        //     if(nums[i] == min || nums[i] == max){
+        //         cnt++;
+        //         flag++;
+        //         if(flag == 2){
+        //             break;
+        //         }
+        //     }
+        //     else{
+        //         cnt++;
+        //     }
+        // }
+        // ans = Math.min(ans , cnt);
+        // cnt = 0;
+        // int cnt1 = 0;
+
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] == min || nums[i] == max){
+        //         cnt++;
+        //         break;
+        //     }
+        //     else{
+        //         cnt++;
+        //     }
+        // }
+
+        // for(int i = nums.length-1; i >= 0; i--){
+        //     if(nums[i] == min || nums[i] == max){
+        //         cnt1++;
+        //         break;    
+        //     }
+        //     else{
+        //         cnt1++;
+        //     }
+        // }
+
+        // ans = Math.min(ans, cnt+cnt1);
+        // return ans;
         
     }
 }
