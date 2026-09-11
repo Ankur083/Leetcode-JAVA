a9e813240528720513048744781144b403432b83
Ankur Raj <ankurjpl07@gmail.com>
2026-09-11 12:53:42 +0530

    Time: 11 ms (37.34%), Space: 47 MB (44.73%) - LeetHub

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
@@ -2,16 +2,9 @@ class Solution {
     public int longestAlternatingSubarray(int[] nums, int threshold) {
         
         int ans = 0;
-        int even = 0;
-        for(int num:nums){
-
-            if(num % 2 == 0 && num <= threshold){
-                even++;
-            }
-            
-        }
         for(int i = 0; i < nums.length; i++){
             if(nums[i] % 2 == 0 && nums[i] <= threshold){
+                ans = Math.max(ans, 1); 
                 for(int j = i+1; j < nums.length; j++){
                     if(nums[j-1] % 2 == nums[j]%2 || nums[j] > threshold){
                         break;
@@ -20,9 +13,6 @@ class Solution {
                     ans = Math.max(ans, j-i+1);
                 }
             }
-        }
-        if(ans == 0 && even > 0){
-            return 1;
         }
         return ans;
 
