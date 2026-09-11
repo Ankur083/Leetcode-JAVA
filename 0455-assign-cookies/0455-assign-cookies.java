fa275b799c7fb1dbe7d019f07b0655050cc576df
Ankur Raj <ankurjpl07@gmail.com>
2026-09-11 12:20:20 +0530

    Time: 18 ms (10.15%), Space: 51.2 MB (87.15%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,21 +1,7 @@
 class Solution {
-    public void reverse(int []s){
-        int i = 0;
-        int j = s.length-1;
-
-        while(i < j){
-            s[i] = s[i]^s[j];
-            s[j] = s[i]^s[j];
-            s[i] = s[i]^s[j];
-
-            i++;
-            j--;
-        }
-    }
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-        // reverse(s);
 
         int cnt = 0;
 
