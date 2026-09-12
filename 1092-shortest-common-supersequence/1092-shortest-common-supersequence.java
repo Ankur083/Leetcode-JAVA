78f86223d2c0c973e0615e9b5feec284fc2d51cb
Ankur Raj <ankurjpl07@gmail.com>
2026-09-12 11:12:43 +0530

    Time: 22 ms (81.81%), Space: 54 MB (50.01%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.java
@@ -5,14 +5,14 @@ class Solution {
         int n2 = s2.length();
         int[][] dp = new int[n1+1][n2+1];
 
-        for (int i = 1; i <= n1; i++) {
-            for (int j = 1; j <= n2; j++) {
+        for (int i = n1-1; i >= 0; i--) {
+            for (int j = n2-1; j >= 0; j--) {
                 int take = 0;
-                if (s1.charAt(i-1) == s2.charAt(j-1)) {
-                    take = 1 + dp[i-1][j-1];
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    take = 1 + dp[i+1][j+1];
                 } else {
-                    int take1 = dp[i-1][j];
-                    int take2 = dp[i][j-1];
+                    int take1 = dp[i+1][j];
+                    int take2 = dp[i][j+1];
                     take =  Math.max(take1, take2);
                 }
                 dp[i][j] = take;
@@ -21,45 +21,45 @@ class Solution {
         }
 
         // for (int i = 0; i < s1.length() + 1; i++) {
-        //     for (int j = 0; j < s2.length() + 1; j++) {
-        //         System.out.print(dp[i][j] + " ");
+            // for (int j = 0; j < s2.length() + 1; j++) {
+            //     System.out.print(dp[i][j] + " ");
 
-        //     }
-        //     System.out.println();
+            // }
+            // System.out.println();
         // }
 
         StringBuilder sb = new StringBuilder();
 
-        int i = n1;
-        int j = n2;
+        int i = 0;
+        int j = 0;
 
-        while (i > 0 && j > 0) {
-            if (s1.charAt(i-1) == s2.charAt(j-1)) {
-                sb.append(s1.charAt(i-1));
-                i--;
-                j--;
+        while (i < n1  && j < n2) {
+            if (s1.charAt(i) == s2.charAt(j)) {
+                sb.append(s1.charAt(i));
+                i++;
+                j++;
             } else {
-                if (dp[i-1][j] > dp[i][j-1]) {
-                    sb.append(s1.charAt(i-1));
-                    i--;
+                if (dp[i+1][j] > dp[i][j+1]) {
+                    sb.append(s1.charAt(i));
+                    i++;
                 } else {
-                    sb.append(s2.charAt(j-1));
-                    j--;
+                    sb.append(s2.charAt(j));
+                    j++;
                 }
             }
         }
 
-        while (i > 0) {
-            sb.append(s1.charAt(i-1));
-            i--;
+        while (i < n1) {
+            sb.append(s1.charAt(i));
+            i++;
         }
 
-        while (j > 0) {
-            sb.append(s2.charAt(j-1));
-            j--;
+        while (j < n2) {
+            sb.append(s2.charAt(j));
+            j++;
         }
 
-        return sb.reverse().toString();
+        return sb.toString();
 
     }
 }
